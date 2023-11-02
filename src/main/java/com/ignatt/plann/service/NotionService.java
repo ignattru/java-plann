6aab378bd5efbd2607cb5865c5b07e6709388683
ignattru <ignatusha.evgeny@gmail.com>
2023-11-02 11:36:24 +0200

    add service for Notion class

---
+++ src/main/java/com/ignatt/plann/service/NotionService.java
@@ -0,0 +1,17 @@
+package com.ignatt.plann.service;
+
+import com.ignatt.plann.entity.Notion;
+
+import java.util.List;
+
+public interface NotionService {
+
+    public List<Notion> getAllNotions();
+
+    public void saveNotion(Notion notion);
+
+    public Notion getNotion(int id);
+
+    public void deleteNotion(int id);
+
+}
---
+++ src/main/java/com/ignatt/plann/service/NotionServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ignatt.plann.service;
+
+import com.ignatt.plann.dao.NotionDAO;
+import com.ignatt.plann.entity.Notion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class NotionServiceImpl implements NotionService {
+
+    @Autowired
+    private NotionDAO notionDAO;
+
+    @Override
+    @Transactional
+    public List<Notion> getAllNotions() {
+        return notionDAO.getAllNotions();
+    }
+
+    @Override
+    @Transactional
+    public void saveNotion(Notion notion) {
+        notionDAO.saveNotion(notion);
+    }
+
+    @Override
+    @Transactional
+    public Notion getNotion(int id) {
+        return notionDAO.getNotion(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteNotion(int id) {
+        notionDAO.deleteNotion(id);
+    }
+}
